2153020a9bf2ca0331a6771d2bf1f8c714ad6a2e
@liqun <liqun@hyperganic.com>
2025-05-18 19:53:42 +0200

    init

---
+++ network/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_id}:role/${var.assume_role}"
+  }
+  profile = "mgmt-account"
+}

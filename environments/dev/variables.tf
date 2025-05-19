5c14c6a334ae3f79f2dd4a53add005857037668c
@liqun <liqun@hyperganic.com>
2025-05-19 12:19:51 +0200

    create network and deploy pipeline to dev

---
+++ environments/dev/variables.tf
@@ -0,0 +1,27 @@
+variable "environment" {
+  type        = string
+  default     = "development"
+  description = "Environment of the infrastructure. Accepted values are development (default), staging and production."
+  nullable    = false
+  sensitive   = false
+  validation {
+    condition     = var.environment == "development" || var.environment == "staging" || var.environment == "production" || can(regex("development-\\w+", var.environment))
+    error_message = "The given deployment level is not accepted. Please select either one of the following three: development, staging, production."
+  }
+}
+
+variable "region" {
+  type        = string
+  default     = "eu-central-1"
+  description = "Selected region to launch the architecture in. Current selection is eu-central-1 (Frankfurt)."
+  nullable    = false
+  sensitive   = true
+}
+
+# AWS Credentials
+variable "account_id" {
+  type        = string
+  description = "AWS Account ID where the resources will be created."
+  nullable    = false
+  sensitive   = true
+}

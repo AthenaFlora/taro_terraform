be652383884582402065ccd01d5c388774e462f8
@AthenaFlora <liqun.ma.2021@gmail.com>
2025-07-02 23:45:55 +0200

    update vpc module refer

--- environments/dev/db.tf
+++ environments/dev/db.tf
@@ -2,7 +2,7 @@ module "db" {
   source               = "../../modules/rds"
   vpc_id               = module.vpc.vpc_id
   db_subnet_group_name = module.vpc.database_subnet_group_name
-  public_subnet_cidrs  = module.vpc.public_subnet_cidrs.cidr_blocks
+  public_subnet_cidrs  = module.vpc.public_subnets_cidr_blocks
   db_username          = var.db_username
   db_password          = var.db_password
 }

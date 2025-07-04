db84e76b1b075fd4f3644c5c9af46fa22f8b086c
@AthenaFlora <liqun.ma.2021@gmail.com>
2025-07-04 18:16:14 +0200

    try run again

--- environments/dev/public.ec2.tf
+++ environments/dev/public.ec2.tf
@@ -3,7 +3,7 @@ module "public_ec2" {
   subnet_id     = module.vpc.public_subnets[0]
   ssh_key_name  = var.public_ec2_ssh_key_name
   enable_public_ip = true
-  security_groups_names = [ module.public_sg.security_group_name ]
+  security_groups_names = module.public_sg.security_group_name
 }
 
 

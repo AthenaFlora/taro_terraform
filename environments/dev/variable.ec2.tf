9c0d2f8243b73d8406f1535b5086b009c624eb3f
AthenaFlora <liqun.ma.22@gmail.com>
2025-07-14 21:39:41 +0200

    change ec2 public key

--- environments/dev/variable.ec2.tf
+++ environments/dev/variable.ec2.tf
@@ -1,5 +1,5 @@
 variable "public_ec2_ssh_key_name" {
   description = "The name of the SSH key pair for public ec2"
   type        = string
-  default = "public_ec2_key_pair"
+  default = "public_key"
 }
